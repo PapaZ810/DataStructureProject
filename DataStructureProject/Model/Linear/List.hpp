5c4c1497ddeee48e49f0866b789ce77f6613c937
Zac <zombiezac@att.net>
2019-02-07 12:18:07 -0700

    made a new template class called list

---
+++ DataStructureProject/Model/Linear/List.hpp
@@ -0,0 +1,30 @@
+//
+//  List.hpp
+//  DataStructureProject
+//
+//  Created by Durham, Zachary on 2/7/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef List_h
+#define List_h
+
+#include "../Nodes/LinearNode.hpp"
+
+template <class Type>
+class Array
+{
+protected:
+    int size;
+public:
+    virtual void add(Type item) = 0;
+    virtual void addAtIndex(int index, Type item) = 0;
+    virtual Type remove(int index) = 0
+    virtual Type getFromIndex(int index) = 0;
+    
+    virtual int getSize() const = 0;
+    virtual LinearNode<Type> * getFront() = 0;
+    virtual LinearNode<Type> * getEnd() = 0;
+}
+
+#endif /* List_h */
