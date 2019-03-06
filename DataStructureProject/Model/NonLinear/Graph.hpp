022aae677ebb87fc92e1f57238d8ea91628aab41
Zac <zombiezac@att.net>
2019-03-06 14:03:38 -0700

    added a little thing

---
+++ DataStructureProject/Model/NonLinear/Graph.hpp
@@ -0,0 +1,26 @@
+//
+//  Graph.hpp
+//  DataStructureProject
+//
+//  Created by Durham, Zachary on 3/6/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Graph_h
+#define Graph_h
+
+#include <set>
+#include <queue>
+#include <assert.h>
+
+using namespace std;
+
+template <class Type>
+class Graph
+{
+private:
+    static const int MAXIMUM = 50;
+     
+};
+
+#endif /* Graph_h */
