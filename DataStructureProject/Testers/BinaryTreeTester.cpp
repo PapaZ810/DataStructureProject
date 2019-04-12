ace028e5b7d2a3539eff2d1dfa34cba23655faf6
Zac <zombiezac@att.net>
2019-04-12 13:52:55 -0600

    added some testers and a tree

---
+++ DataStructureProject/Testers/BinaryTreeTester.cpp
@@ -0,0 +1,14 @@
+//
+//  BinaryTreeTester.cpp
+//  DataStructureProject
+//
+//  Created by Durham, Zachary on 4/12/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "BinaryTreeTester.hpp"
+
+void BinaryTreeTester :: treeStuff()
+{
+    
+}
---
+++ DataStructureProject/Testers/BinaryTreeTester.hpp
@@ -0,0 +1,24 @@
+//
+//  BinaryTreeTester.hpp
+//  DataStructureProject
+//
+//  Created by Durham, Zachary on 4/12/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeTester_hpp
+#define BinaryTreeTester_hpp
+
+#include "../Model/Nodes/BinaryTreeNode.h"
+//#include "../Model/NonLinear/BinarySearchTree.hpp"
+
+class BinaryTreeTester
+{
+private:
+    BinaryTreeNode<int> testNode;
+    //BinarySearchTree<int> testNode;
+public:
+    void treeStuff();
+};
+
+#endif /* BinaryTreeTester_hpp */
