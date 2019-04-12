0ed73b0248fd701e1f33af3a606abc29e7986167
Zac <zombiezac@att.net>
2019-04-12 12:44:02 -0600

    created the binarytreenode header file and added the constructors

---
+++ DataStructureProject/Model/Nodes/BinaryTreeNode.h
@@ -0,0 +1,48 @@
+//
+//  BinaryTreeNode.h
+//  DataStructureProject
+//
+//  Created by Durham, Zachary on 4/12/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeNode_h
+#define BinaryTreeNode_h
+
+#include "Node.hpp"
+
+template <class Type>
+class BinaryTreeNode : public Node<Type>
+{
+private:
+    BinaryTreeNode<Type> * root;
+    BinaryTreeNode<Type> * leftChild;
+    BinaryTreeNode<Type> * rightChild;
+public:
+    BinaryTreeNode<Type>();
+    BinaryTreeNode<Type>(Type);
+    
+    BinaryTreeNode<Type> * getRootNode();
+    BinaryTreeNode<Type> * getLeftChild();
+    BinaryTreeNode<Type> * getRightChild();
+    
+    void setRootNode(BinaryTreeNode<Type> * root);
+    void setLeftChild(BinaryTreeNode<Type> * leftChild);
+    void setRightChild(BinaryTreeNode<Type> * rightChild);
+};
+
+template <class Type>
+BinaryTreeNode<Type> :: BinaryTreeNode() : Node<Type>()
+{
+    
+}
+
+template <class Type>
+BinaryTreeNode<Type> :: BinaryTreeNode(Type data) : Node<Type>()
+{
+    root = nullptr;
+    leftChild = nullptr;
+    rightChild = nullptr;
+}
+
+#endif /* BinaryTreeNode_h */
